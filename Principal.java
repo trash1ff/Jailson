75a4dfc158e318253fffefc553c1cfb642bccea8
Gabriel Jung <115058662+trash1ff@users.noreply.github.com>
2022-10-31 23:15:09 -0400

    Add files via upload

---
+++ Principal.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		Conta c1 = new Conta(123,"Poupança","Jailson",2000);
+		
+		System.out.println(c1.getDescricao());
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Escolha a opção: Saque(1) ou Deposito(2) !");
+		int numero = entrada.nextInt();
+		switch(numero){
+			case 1:
+		       System.out.println("Opção Escolhida: Saque !");
+		       
+		       System.out.println("Digite o valor do Saque: ");
+		       c1.saque = scan.nextDouble();
+		       
+		       c1.getSaque();
+		       System.out.println("Olá " + c1.nome + " voçê fez um saque de: " +c1.saque+ " Resta em sua conta: " +c1.saldo + " R$.");
+		       
+		       break;
+		       
+		       
+		       
+		     case 2:
+		       System.out.println("Opção Escolhida: Deposito !");
+		       
+		       System.out.println("Digite o valor do Deposito: ");
+		       c1.deposito = scan.nextDouble();
+		       
+		       c1.getDeposito();
+		       System.out.println("Olá " + c1.nome + " voçê fez um Deposito de: " +c1.deposito+ " Possui em sua conta: " +c1.saldo + " R$.");
+		       
+		       break;
+		     default:
+		         System.out.println("Opção Invalida ! Escolha a opção: Saque(1) ou Deposito(2) !");
+		}
+			
+	}
+
+}
