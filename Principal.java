796aad8fe61015755d36e3084dbc181f23b8e42a
Gabriel Jung <115058662+trash1ff@users.noreply.github.com>
2022-11-01 01:48:32 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -19,9 +19,14 @@ public class Principal {
 		       System.out.println("Digite o valor do Saque: ");
 		       c1.saque = scan.nextDouble();
 		       
+		       if(c1.saque > c1.saldo){
+		    	   System.out.println("Você não tem Dinheiro o Suficiente!");
+		       }
+		       
+		       if(c1.saque <= c1.saldo){
 		       c1.getSaque();
 		       System.out.println("Olá " + c1.nome + " voçê fez um saque de: " +c1.saque+ " Resta em sua conta: " +c1.saldo + " R$.");
-		       
+		       }
 		       break;
 		       
 		       
@@ -32,10 +37,16 @@ public class Principal {
 		       System.out.println("Digite o valor do Deposito: ");
 		       c1.deposito = scan.nextDouble();
 		       
+		       if(c1.deposito <= 0) {
+		    	   System.out.println("O deposito não pode ser menor ou igual a 0 R$");
+		       }
+		       
+		       if(c1.deposito > 0){
 		       c1.getDeposito();
 		       System.out.println("Olá " + c1.nome + " voçê fez um Deposito de: " +c1.deposito+ " Possui em sua conta: " +c1.saldo + " R$.");
-		       
+		       }
 		       break;
+		       
 		     default:
 		         System.out.println("Opção Invalida ! Escolha a opção: Saque(1) ou Deposito(2) !");
 		}
